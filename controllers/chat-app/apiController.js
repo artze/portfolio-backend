e267e13374eb20ca283e9d7524865d4e2445f9ff
artze <arthurwtm@gmail.com>
2018-09-30 14:01:26 +0200

    Assign uuid to each client; Broadcast message to all clients accept message originator

--- controllers/chat-app/apiController.js
+++ controllers/chat-app/apiController.js
@@ -1,9 +1,21 @@
 const expressWsInstance = require('../../websocket');
+const uuidv4 = require('uuid/v4');
 
 const relayMessage = (ws, req) => {
+
+    // Register client id
+    expressWsInstance.getWss().on('connection', (ws) => {
+        ws.id = uuidv4();
+        console.log('connected')
+    })
+
     ws.on('message', (msg) => {
-        console.log(expressWsInstance.getWss().clients)
-        ws.send(msg)
+        expressWsInstance.getWss().clients.forEach((client) => {
+            if(client.id === ws.id) {
+                return;
+            }
+            client.send(msg);
+        })
     })
 }
 
