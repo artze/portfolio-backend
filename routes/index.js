c0420f7012ac8b9683d710bdd37b5f91afc8f14e
artze <arthurwtm@gmail.com>
2019-02-02 13:08:35 +0000

    Insert cam-test page

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,20 @@
 const journeyTrackerRoutes = require('./journey-tracker');
 const chatAppRoutes = require('./chat-app');
 
+// REMOVE BELOW THIS LINE
+const path = require('path');
+// REMOVE ABOVE THIS LINE
+
 const init = (app) => {
     app.use('/api/journey-tracker', journeyTrackerRoutes);
 
     app.use('/api/chat-app', chatAppRoutes);
+
+    // REMOVE BELOW THIS LINE
+    app.get('/api/cam-test', function(req, res) {
+        res.sendFile(path.join(path.dirname(require.main.filename || process.mainModule.filename) + '/public/index.html'));
+    })
+    // REMOVE ABOVE THIS LINE
 }
 
 module.exports = {
